4be6282e4ed3fbcc2ae7aec9876a08b78d7ff694
Akshay1718 <86643106+Akshay1718@users.noreply.github.com>
2022-08-23 13:17:13 +0530

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,1 +1,1 @@
-CREATE TABLE aks.Student_Details(Name VARCHAR(255) NOT NULL,(20) primary key,Roll_no int(5) not null, Year_of_Study int(1) not null,Department VARCHAR(10));
+CREATE TABLE aks.Student_Details(Name VARCHAR(20) primary key,Roll_no int(5) not null, Year_of_Study int(1) not null,Department VARCHAR(10));
